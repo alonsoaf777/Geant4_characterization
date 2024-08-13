650b0cb75f088b6e5767f9a846837422b1eecce8
Alonso AF <71473449+alonsoaf777@users.noreply.github.com>
2024-08-13 14:59:22 -0600

    Other way to implement detectorConstruction

---
+++ Geant4_singleWall2/src/crossrun.cc
@@ -0,0 +1,68 @@
+#include "crossrun.hh"
+
+CrossRun::CrossRun()
+{}
+
+CrossRun::~CrossRun()
+{}
+
+void CrossRun::SetPrimary(G4ParticleDefinition *particle, G4double energy)
+{
+	fParticle = particle; 
+	fEkin = energy; 
+}
+
+void CrossRun::CountProcesses(G4String procName)
+{
+	std::map<G4String, G4int>::iterator it = fProcCounter.find(procName); 
+	if (it == fProcCounter.end())
+	{
+		fProcCounter[procName] = 1; 
+	}
+	else
+	{
+		fProcCounter[procName]++; 
+	}
+}
+
+//void Merge
+
+void CrossRun::EndOfRun()
+{
+	//Ni idea xd
+	G4int prec = 5; 
+	G4int dfprec = G4cout.precision(prec);
+	
+	//Obtain the material properties of the target and particle gun
+	//Create an object to store the thickness
+	const MyDetectorConstruction *detectorConstruction = static_cast<const MyDetectorConstruction*> (G4RunManager::GetRunManager()->GetUserDetectorConstruction()); 
+	G4Material *material = detectorConstruction->GetMaterial(); 
+	G4double density = material->GetDensity(); 
+	
+	G4String partName = fParticle->GetParticleName(); 
+	G4double thickness = detectorConstruction->GetThickness(); 
+	
+	G4cout << "\n ======================== run summary ======================\n";
+ 	G4cout << "\n The run is: " << numberOfEvent << " " << partName << " of "
+        << G4BestUnit(fEkin,"Energy") << " through " 
+        << G4BestUnit(thickness,"Length") << " of "
+        << material->GetName() << " (density: " 
+        << G4BestUnit(density,"Volumic Mass") << ")" << G4endl;
+        
+        //Frecuency
+        //frequency of processes
+	  G4int totalCount = 0;
+	  G4int survive = 0;  
+	  G4cout << "\n Process calls frequency --->";
+	  std::map<G4String,G4int>::iterator it;  
+	  for (it = fProcCounter.begin(); it != fProcCounter.end(); it++) {
+	     G4String procName = it->first;
+	     G4int    count    = it->second;
+	     totalCount += count; 
+	     G4cout << "\t" << procName << " = " << count;
+	     if (procName == "Transportation") survive = count;
+	  }
+	  G4cout << G4endl;
+        
+        
+}
---
+++ Geant4_singleWall2/src/run.cc
@@ -0,0 +1,71 @@
+#include "run.hh"
+
+MyRunAction::MyRunAction()
+{
+	//Instantiate the analysis manager for outputs
+	G4AnalysisManager *man = G4AnalysisManager::Instance(); 
+	
+	//Create the root data for energy deposition
+	man->CreateNtuple("Scoring", "Scoring"); 
+	man->CreateNtupleDColumn("fEdep"); 
+	man->FinishNtuple(0);
+	//For hits
+	man->CreateNtuple("Hits", "Hits"); 
+	man->CreateNtupleIColumn("fEvent"); 
+	man->CreateNtupleDColumn("fX"); 
+	man->CreateNtupleDColumn("fY"); 
+	man->CreateNtupleDColumn("fZ");
+	man->FinishNtuple(1); 
+	
+	//Create histogram for cross section
+	man->CreateNtuple("CrossSection", "CrossSection");
+	man->CreateNtupleDColumn("Cross/Volume cm^-1");
+	man->CreateNtupleDColumn("Cross/Mass ");
+	man->FinishNtuple(2); 
+	 
+}
+
+MyRunAction::~MyRunAction()
+{}
+
+//Generate the run of cross section
+G4Run *MyRunAction::GenerateRun()
+{
+	fRun = new CrossRun(); 
+	return fRun;
+}
+
+void MyRunAction::BeginOfRunAction(const G4Run* run)
+{
+	//Create an object to store the thickness
+	const MyPrimaryGenerator *primaryGenerator = static_cast<const MyPrimaryGenerator*> (G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction()); 
+	
+	G4AnalysisManager *man = G4AnalysisManager::Instance(); 
+	
+	//Generate a root file with its individual run
+	G4int runID = run->GetRunID(); 
+	
+	std::stringstream strRunID; 
+	strRunID << runID; 
+	
+	man->OpenFile("output" + strRunID.str() + ".root"); 
+	
+	if (primaryGenerator)
+	{
+		G4ParticleDefinition *particle = primaryGenerator->GetParticleGun()->GetParticleDefinition(); 
+		G4double energy = primaryGenerator->GetParticleGun()->GetParticleEnergy();
+		fRun->SetPrimary(particle, energy);
+ 
+	}
+	
+}
+
+void MyRunAction::EndOfRunAction(const G4Run*)
+{
+	fRun->EndOfRun();
+	
+	G4AnalysisManager *man = G4AnalysisManager::Instance(); 
+	
+	man->Write(); 
+	man->CloseFile(); 
+}
