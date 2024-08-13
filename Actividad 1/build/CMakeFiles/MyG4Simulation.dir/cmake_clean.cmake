8ff402b8774444d42f5ee6bff950516ae73229ec
miguelcomett <A01735935@tec.mx>
2024-08-13 15:24:36 -0600

    Delete Actividad 1/build directory

--- Actividad 1/build/CMakeFiles/MyG4Simulation.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MyG4Simulation"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MyG4Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
